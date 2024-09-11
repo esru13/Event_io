8bf69a89257215b06d076631449ecb44f2691da0
esru13 <esraelb313@gmail.com>
2024-09-12 02:36:49 +0300

    admin middleware

---
+++ Back-End/Event-IO/app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(Auth::check() && Auth::user()->role === 'admin'){
+            return $next($request);
+        }
+
+        return response()->json(['message'=>'unauthorized, only admins can perform this action'], 401);
+
+    }
+}
