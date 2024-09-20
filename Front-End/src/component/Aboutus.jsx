df1871a9c5e7ea44efc44581eb2b19b7855a3a6c
Beka Tilahun <bekatkopessa831@gmail.com>
2024-09-20 10:45:54 +0300

    Aboutus, Features, How it works added

---
+++ Front-End/src/component/Aboutus.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="py-20 px-8 bg-[#F3F4FF] text-black text-center">
+      <h1 className="text-4xl font-bold mb-5">
+        About <span className="text-[#5516DA]">Us</span>
+      </h1>
+      <p className="text-lg leading-relaxed mx-auto max-w-prose">
+        At Eventio, we believe in the power of connection and shared experiences. Our platform is designed to bring people together by providing easy access to a wide range of events, from concerts and art exhibitions to sports games and community gatherings. With a user-friendly interface, Eventio allows you to explore events that match your interests, purchase tickets seamlessly, and stay updated on the latest happenings in your area. Our mission is to enhance your event experience by offering personalized recommendations and exclusive access to unique opportunities. Join us in discovering and celebrating the vibrant events that enrich our lives and foster community connections.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
--- Front-End/src/pages/landing/landing.jsx
+++ Front-End/src/pages/landing/landing.jsx
@@ -1,19 +1,25 @@
 import React from "react";
+import Navbar from "../../component/Navbar";
 import Banner from "../../component/Banner";
-import Footer from "../../component/Footer";
-import Navbar from "../../component/Navbar"; 
-import EventTicketFAQ from "../../component/EventTicketFAQ";
-import Testimonial from "../../component/Testimonial";
-import Brand from "../../component/Brand"; 
 import Belowbanner from "../../component/Belowbanner";
+import Features from "../../component/Features";
+import Brand from "../../component/Brand";
+import Testimonial from "../../component/Testimonial";
+import EventTicketFAQ from "../../component/EventTicketFAQ";
+import Footer from "../../component/Footer";
+import How from "../../component/How"; // Import the combined How component
+import AboutUs from "../../component/Aboutus";
 
 function Landing() {
   return (
     <>
-      <Navbar /> 
+      <Navbar />
       <Banner />
       <Belowbanner />
-      <Brand /> 
+      <Features />
+      <How /> {/* Use the How component */}
+      <Brand />
+      <AboutUs />
       <Testimonial />
       <EventTicketFAQ />
       <Footer />
